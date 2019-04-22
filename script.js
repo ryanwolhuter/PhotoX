551cbb63e2fe30b2a0e1f4ac54a3c5bf3a012929
ryanwolhuter <dev@ryanwolhuter.com>
2019-04-22 15:08:55 +0200

    Add gallery

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ $(document).ready(function () {
             $('.nav-menu').removeClass('custom-navbar')
         }
     })
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop()
+        if (position >= 650) {
+            $('.camera-img').addClass('fromLeft')
+            $('.mission-text').addClass('fromRight')
+        } else {
+            $('.camera-img').removeClass('fromLeft')
+            $('.mission-text').removeClass('fromRight')
+        }
+    })
+
+    $('.gallery-list-item').click(function () {
+        let value = $(this).attr('data-filter')
+        if (value === 'all') {
+            $('.filter').show(300)
+        } else {
+            $('.filter').not('.' + value).hide(300)
+            $('.filter').filter('.' + value).show(300)
+        }
+    })
+
+    $('.gallery-list-item').click(function () {
+        $(this).addClass('active-item').siblings().removeClass('active-item')
+    })
 })
 
 
